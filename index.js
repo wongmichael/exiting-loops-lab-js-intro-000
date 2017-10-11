2fed84733a2ff8492e22ee743a6a463d57ca0d99
Michael Wong <mik9080@gmail.com>
2017-10-11 19:02:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function breakOut(array,changeValue,stopValue){
   array.forEach()
 }
 function keepGoing(array,changeValue,skipValue){
-  array.forEach((e)=>{e==skipValue?continue:changeValue})
-  return array
+  array.forEach()
 }
 function findBy(array,findFn){
 
